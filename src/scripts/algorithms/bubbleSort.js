457dc99d1728095dd182cf83fb930ce00c9faa59
toyama <toyama@toyama1710.net>
2025-01-31 08:03:32 +0900

    クエリ形式を合わせた

--- src/scripts/algorithms/bubbleSort.js
+++ src/scripts/algorithms/bubbleSort.js
@@ -2,6 +2,9 @@ function bubbleSort(arr) {
     let n = arr.length;
     let orderList = []; // アニメーション用の操作履歴を保存するリスト
 
+    orderList.push([DESCRIBE, "init"]);
+    orderList.push([ARR, "arr", [...arr]]); // 配列を明示的にコピーしておかないと、後のソートの影響をうける
+
     for (let i = 0; i < n - 1; i++) {
         let swapped = false;
         for (let j = 0; j < n - i - 1; j++) {
@@ -12,7 +15,7 @@ function bubbleSort(arr) {
                 arr[j + 1] = temp;
 
                 // 操作を記録
-                let order = ["swap", j, j + 1];
+                let order = ["swap", `arr[${j}]`, `arr[${j + 1}]`];
                 orderList.push(order);
 
                 swapped = true;
